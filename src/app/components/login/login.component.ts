ca1eb6e3df4bae6b3df1e0c2bc9293264979c1e7
kdiamantis <kdiamantis@iknowhow.com>
2025-03-01 21:22:10 +0200

    Validations in case of not filling for email and password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [CardModule, InputTextModule, ReactiveFormsModule],
+  imports: [CardModule, InputTextModule, ReactiveFormsModule, ButtonModule, RouterModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  loginForm!: FormGroup;
+  loginForm: FormGroup;
 
-  ngOnInit(): void {
-    this.loginForm = new FormGroup({
-      email: new FormControl('')
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
   }
-  
 
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
 }
 
